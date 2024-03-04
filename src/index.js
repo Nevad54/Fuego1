48bcba9d66f130c5abe7236922eab60e3c1d0ec0
Nevad54 <82282630+Nevad54@users.noreply.github.com>
2024-03-04 12:38:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -59,13 +59,9 @@ const mongoHost = 'mongodb+srv://systembfp8:iwantaccess@bfp.ezea3nm.mongodb.net/
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Types; 
 // Start HTTP server
-httpServer.listen(port, WebURL, () => {
-  console.log(`Server listening at http://${WebURL}:${port}.`);
-})
-  .on('error', (err) => {
-      console.error(`Error starting HTTP server: ${err.message}`);
-      // Handle HTTP server start error here
-  });
+httpServer.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening at http://0.0.0.0:${port}.`);
+});
 
 
 // Connect to MQTT
