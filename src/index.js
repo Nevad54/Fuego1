9f25f2326996ed675f0d8a22f07339f60bfd97ff
Nevad54 <82282630+Nevad54@users.noreply.github.com>
2024-03-05 02:42:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// index.js
 const multer = require('multer');
 const express = require("express");
 const path = require("path");
@@ -45,7 +46,6 @@ const isLoggedIn = (req, res, next) => {
 app.use(cors({
   origin: '*',
 }));
-
 const httpServer = http.createServer(app);
 const mqttClient = mqtt.connect(mqttOptions);
 const port = process.env.PORT || 5000;
@@ -54,20 +54,20 @@ const io = require('socket.io')(httpServer, {
     origin: '*',
   },
 });
+const WebURL = 'fuego1.onrender.com';
+
 
 const mongoHost = 'mongodb+srv://systembfp8:iwantaccess@bfp.ezea3nm.mongodb.net/?retryWrites=true&w=majority/accounts';
 const mongoose = require('mongoose');
-const { ObjectId } = mongoose.Types;
-
+const { ObjectId } = mongoose.Types; 
 // Start HTTP server
-httpServer.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+httpServer.listen(port, WebURL, () => {
+  console.log(`Server listening at https://${WebURL}:${port}.`);
 })
-.on('error', (err) => {
-  console.error(`Error starting HTTP server: ${err.message}`);
-  // Handle HTTP server start error here
-});
-
+  .on('error', (err) => {
+      console.error(`Error starting HTTP server: ${err.message}`);
+      // Handle HTTP server start error here
+  });
 
 // Connect to MQTT
 mqttClient.on('connect', () => {
