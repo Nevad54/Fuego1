fa80099876fda721de5313e964716847a404bb4e
Nevad54 <82282630+Nevad54@users.noreply.github.com>
2024-03-05 02:27:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ const isLoggedIn = (req, res, next) => {
 app.use(cors({
   origin: '*',
 }));
+
 const httpServer = http.createServer(app);
 const mqttClient = mqtt.connect(mqttOptions);
 const port = process.env.PORT || 5000;
@@ -54,20 +55,23 @@ const io = require('socket.io')(httpServer, {
     origin: '*',
   },
 });
-const WebURL = 'fuego1.onrender.com';
 
+// Declare WebURL here
+const WebURL = 'fuego1.onrender.com';
 
 const mongoHost = 'mongodb+srv://systembfp8:iwantaccess@bfp.ezea3nm.mongodb.net/?retryWrites=true&w=majority/accounts';
 const mongoose = require('mongoose');
-const { ObjectId } = mongoose.Types; 
-// Start HTTP server// Start HTTP server
+const { ObjectId } = mongoose.Types;
+
+// Start HTTP server
 httpServer.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+  console.log(`Server listening at https://${WebURL}:${port}.`);
 })
 .on('error', (err) => {
   console.error(`Error starting HTTP server: ${err.message}`);
   // Handle HTTP server start error here
 });
+
 
 // Connect to MQTT
 mqttClient.on('connect', () => {
