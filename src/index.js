e3aae6744a61a455673e7fa6687ea48bc4d63981
Nevad54 <82282630+Nevad54@users.noreply.github.com>
2024-03-04 12:12:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ app.use(cors({
 }));
 const httpServer = http.createServer(app);
 const mqttClient = mqtt.connect(mqttOptions);
-const port = 5000;
+const port = 49152;
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: '*',
